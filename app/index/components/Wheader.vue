c99bb96b698c41fe045bba5c48ffa73ff7bead85
Wpf <851118434@qq.com>
2017-08-15 18:02:35 +0800

    add

---
+++ app/index/components/Wheader.vue
@@ -0,0 +1,20 @@
+<template>
+	<header>
+		<a href="">返回</a>
+		<h1>标题</h1>
+		<a>功能</a>
+	</header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style>
+ </style>
--- app/index/index.js
+++ app/index/index.js
@@ -1,9 +1,10 @@
 //import Vue from 'Vue';  
 import Vue from 'vue/dist/vue.js';
+import WHeader from './components/Wheader';
 import Favlist from './components/Favlist'
 
 console.log(Vue);
 new Vue({
     el: '#app',
-    components: { Favlist }
+    components: { Favlist}
 })
